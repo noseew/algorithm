788327481b0ab735390d577ec7337dade37c14f6
noseew <noseew@qq.com>
2021-04-07 21:03:52 +0800

    leecode

--- leetcode-demo/src/test/java/algorithmbase/leetcode/midle/Leetcode_05_LongestPalindrome.java
+++ leetcode-demo/src/test/java/algorithmbase/leetcode/midle/Leetcode_05_LongestPalindrome.java
@@ -39,7 +39,8 @@ public class Leetcode_05_LongestPalindrome {
     @Test
     public void test() {
         // "xaabacxcabaax"
-        System.out.println(longestPalindrome("xaabacxcabaaxcabaax"));
+//        System.out.println(longestPalindrome2("xaabacxcabaaxcabaax"));
+        System.out.println(longestPalindrome2("cbbd"));
         System.out.println();
     }
 
@@ -93,6 +94,73 @@ public class Leetcode_05_LongestPalindrome {
                         startIndexTemp = i - j;
                     } else {
                         break;
+                    }
+                }
+                if (maxLenTemp > maxLen) {
+                    startIndex = startIndexTemp;
+                    maxLen = maxLenTemp;
+                }
+            }
+
+        }
+
+        return s.substring(startIndex, startIndex + maxLen);
+    }
+
+    public String longestPalindrome2(String s) {
+
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        char[] chars = s.toCharArray();
+
+        if (chars.length == 2 && chars[0] != chars[1]) {
+            return String.valueOf(chars[0]);
+        }
+
+        int startIndex = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (maxLen == chars.length) {
+                break;
+            }
+
+            if (i >= 1 && chars[i] == chars[i - 1]) {
+                // 偶对称
+                int maxLenTemp = 0;
+                int startIndexTemp = i - 1;
+                int maxJ = Math.min(chars.length - i, i);
+                int maxAble = maxLenTemp + maxJ * 2;
+                if (maxAble > maxLen) {
+                    for (int j = 0; j < maxJ; j++) {
+                        if (chars[i - j - 1] != chars[i + j]) {
+                            break;
+                        }
+                        maxLenTemp += 2;
+                        startIndexTemp = i - j - 1;
+                    }
+                }
+                if (maxLenTemp > maxLen) {
+                    startIndex = startIndexTemp;
+                    maxLen = maxLenTemp;
+                }
+            }
+            if (i > 0 && i < chars.length - 1
+                    && chars[i - 1] == chars[i + 1]) {
+                // 奇对称
+                int maxLenTemp = 1;
+                int startIndexTemp = i;
+                int maxJ = Math.min(chars.length - i - 1, i);
+                int maxAble = maxLenTemp + maxJ * 2;
+                if (maxAble > maxLen) {
+                    for (int j = 1; j <= maxJ; j++) {
+                        if (chars[i + j] != chars[i - j]) {
+                            break;
+                        }
+                        maxLenTemp += 2;
+                        startIndexTemp = i - j;
                     }
                 }
                 if (maxLenTemp > maxLen) {
