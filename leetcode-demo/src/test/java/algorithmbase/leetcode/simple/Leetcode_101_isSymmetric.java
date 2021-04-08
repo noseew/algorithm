264becd0fe3b2084602efc6207f6c40430a216da
noseew <noseew@qq.com>
2021-04-08 22:08:23 +0800

    leecode

--- leetcode-demo/src/test/java/algorithmbase/leetcode/simple/Leetcode_101_isSymmetric.java
+++ leetcode-demo/src/test/java/algorithmbase/leetcode/simple/Leetcode_101_isSymmetric.java
@@ -1,22 +1,53 @@
 package algorithmbase.leetcode.simple;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 101. 对称二叉树
  * 给定一个二叉树，检查它是否是镜像对称的。
- *
+ * <p>
  * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
- *
+ * <p>
  * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
  * 进阶：
- *
+ * <p>
  * 你可以运用递归和迭代两种方法解决这个问题吗？
  */
 public class Leetcode_101_isSymmetric {
 
+    @Test
+    public void test() {
+        System.out.println(isSymmetric(
+                new TreeNode(1,
+                        new TreeNode(2, new TreeNode(3), null),
+                        new TreeNode(2, null, new TreeNode(3)))));
+        System.out.println();
+    }
+
     public boolean isSymmetric(TreeNode root) {
-        TreeNode p = root.left;
-        TreeNode q = root.right;
 
+        List<TreeNode> pList = new ArrayList<>();
+        preOrderTraveral(root.left, pList);
+
+        List<TreeNode> qList = new ArrayList<>();
+        postOrderTraveral(root.right, qList);
+
+        if (pList.size() != qList.size()) {
+            return false;
+        }
+        for (int i = 0; i < pList.size(); i++) {
+            if (!isSymmetricUtil(pList.get(i), qList.get(pList.size() - i - 1), true)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public boolean isSymmetricUtil(TreeNode p, TreeNode q, boolean recursion) {
         if (p == null && q == null) {
             return true;
         }
@@ -29,42 +60,28 @@ public class Leetcode_101_isSymmetric {
         if (p.val != q.val) {
             return false;
         }
-        return isSymmetric(p) && isSymmetric(q);
-    }
-
-
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
+        if (!recursion) {
             return true;
         }
-        if (p == null && q != null) {
-            return false;
-        }
-        if (q == null && p != null) {
-            return false;
-        }
-        if (p.val != q.val) {
-            return false;
-        }
-        return isSameTree(p.left, q.right) && isSameTree(p.right, q.left);
+        return isSymmetricUtil(p.left, q.right, false) && isSymmetricUtil(p.right, q.left, false);
     }
 
-    public static void preOrderTraveral(TreeNode node) {
+    public static void preOrderTraveral(TreeNode node, List<TreeNode> nodes) {
         if (node == null) {
             return;
         }
-        System.out.print(node.val + " ");
-        preOrderTraveral(node.left);
-        preOrderTraveral(node.right);
+        nodes.add(node);
+        preOrderTraveral(node.left, nodes);
+        preOrderTraveral(node.right, nodes);
     }
 
-    public static void postOrderTraveral(TreeNode node) {
+    public static void postOrderTraveral(TreeNode node, List<TreeNode> nodes) {
         if (node == null) {
             return;
         }
-        postOrderTraveral(node.left);
-        postOrderTraveral(node.right);
-        System.out.print(node.val + " ");
+        postOrderTraveral(node.left, nodes);
+        postOrderTraveral(node.right, nodes);
+        nodes.add(node);
     }
 
     /**
