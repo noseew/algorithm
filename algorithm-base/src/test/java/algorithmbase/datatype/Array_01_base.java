c5f35eacad44674667e4996ffb5572c163694a72
noseew <noseew@qq.com>
2021-04-18 21:52:10 +0800

    datatype

---
+++ algorithm-base/src/test/java/algorithmbase/datatype/Array_01_base.java
@@ -0,0 +1,87 @@
+package algorithmbase.datatype;
+
+import java.util.Arrays;
+
+public class Array_01_base<T> {
+
+    private T[] datas;
+
+    private int size;
+
+    public Array_01_base(int capacity) {
+        datas = (T[]) new Object[capacity];
+    }
+
+    public Array_01_base() {
+        this(10);
+    }
+
+    public void add(T data) {
+        ensureCapacity();
+        datas[++size - 1] = data;
+    }
+
+    public T remove(int index) {
+        if (index <= size - 1) {
+            T oldVal = datas[index];
+            for (int j = index; j < size - 1; j++) {
+                datas[j] = datas[j + 1];
+            }
+            size--;
+            return oldVal;
+        }
+        return null;
+    }
+
+    public T remove(T value) {
+        if (value == null) {
+            for (int i = 0; i < size; i++) {
+                if (datas[i] == null) {
+                    for (int j = i; j < size - 2; j++) {
+                        datas[j] = datas[j + 1];
+                    }
+                    size--;
+                    return null;
+                }
+            }
+            return null;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (datas[i] == value) {
+                    T oldVal = datas[i];
+                    for (int j = i; j < size - 2; j++) {
+                        datas[j] = datas[j + 1];
+                    }
+                    size--;
+                    return oldVal;
+                }
+            }
+            return null;
+
+        }
+    }
+
+    /**
+     * 确保容量
+     */
+    private void ensureCapacity() {
+        if (size >= datas.length - 1) {
+            dilatation();
+        }
+    }
+
+    /**
+     * 扩容
+     */
+    private void dilatation() {
+        T[] newData = (T[]) new Object[datas.length + (datas.length >> 1)];
+        System.arraycopy(datas, 0, newData, 0, datas.length);
+        datas = newData;
+    }
+
+    @Override
+    public String toString() {
+        return "size=" + size +
+                "\r\ndatas=" + Arrays.toString(datas);
+    }
+}
---
+++ algorithm-base/src/test/java/algorithmbase/datatype/Array_01_base_test.java
@@ -0,0 +1,20 @@
+package algorithmbase.datatype;
+
+import org.junit.Test;
+
+public class Array_01_base_test {
+
+    @Test
+    public void test() {
+        Array_01_base<Integer> list = new Array_01_base<>(5);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list.toString());
+        list.remove(2);
+        System.out.println(list.toString());
+
+    }
+}
