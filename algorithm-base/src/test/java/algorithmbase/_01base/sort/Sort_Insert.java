320e659f26b8c45cf854963f8e0a601df436881b
noseew <noseew@qq.com>
2021-04-18 09:18:30 +0800

    sort

--- algorithm-base/src/test/java/algorithmbase/_01base/sort/Sort_Insert.java
+++ algorithm-base/src/test/java/algorithmbase/_01base/sort/Sort_Insert.java
@@ -12,26 +12,37 @@ public class Sort_Insert {
 
     @Test
     public void test() {
-        int[] arr = new int[]{5, 4, 3, 2, 1};
+        int[] arr = new int[]{4, 5, 2, 0};
         System.out.println(Arrays.toString(sort_01(arr)));
     }
 
     /**
-     * 基础版本
      */
     public static int[] sort_01(int[] data) {
-        int sortedMaxIndex = 0;
+        int count = 0;
+        int countOuter = 0;
+
         for (int i = 1; i < data.length; i++) {
-            for (int j = 0; j < i; j++) {
+            int countInner = 0;
+            System.out.println(String.format("外部循环: 次数 %s, 结果: %s", countOuter++, Arrays.toString(data)));
 
+            int cindex = i;
+            int cdata = data[i];
+            for (int j = i - 1; j >= 0; j--) {
+                if (cdata < data[j]) {
+                    int temp = data[j];
+                    data[j] = cdata;
+                    data[cindex] = temp;
+                    cindex = j;
 
-                if (data[i] > data[j]) {
-                    int temp = data[i];
-                    data[i] = data[j];
-                    data[j] = temp;
+                    count++;
+                    System.out.println(String.format("\t内部循环: 次数 %s, 结果: %s", countInner++, Arrays.toString(data)));
+                } else {
+                    break;
                 }
             }
         }
+        System.out.println(String.format("总次数 %s", count));
         return data;
     }
 
