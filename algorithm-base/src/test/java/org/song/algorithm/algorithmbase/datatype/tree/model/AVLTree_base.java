7a24288fbe48809eedb89faf02b8d7ec770a56c6
加一 <noseew@qq.com>
2021-04-23 17:44:32 +0800

    datatype

--- algorithm-base/src/test/java/org/song/algorithm/algorithmbase/datatype/tree/model/AVLTree_base.java
+++ algorithm-base/src/test/java/org/song/algorithm/algorithmbase/datatype/tree/model/AVLTree_base.java
@@ -18,6 +18,8 @@ public class AVLTree_base<V> {
     public void push(V v) {
         if (root == null) {
             root = new AVLTreeNode<>(null, null, null, v);
+            // 叶子节点的高度都是0
+            root.height = 0;
             size++;
             return;
         }
@@ -41,6 +43,8 @@ public class AVLTree_base<V> {
             if (next == null) {
                 break;
             }
+            // 遍历过的节点, 高度+1
+            next.height++;
             parent = next;
         }
 
@@ -68,20 +72,20 @@ public class AVLTree_base<V> {
     private void balance(AVLTreeNode<V> node) {
         /*
          平衡二叉树的平衡分为
-         RR: 单向右型: 需要左旋操作
-         LR: 左右型: 先左旋再右旋
-         LL: 单向左型: 需要右旋操作
-         RL: 右左型: 先右旋再左旋
+         RR: 单向右型 / : 需要左旋操作
+         LR: 左右型   < : 先左旋再右旋
+         LL: 单向左型 / : 需要右旋操作
+         RL: 右左型   > : 先右旋再左旋
          */
     }
 
     /**
-     * 右旋
-     * 针对 LL
+     * 右旋 处理 / 型不平衡
      *
      * @param node 不平衡的节点, isBalanced(node) = false
+     * @return 新的 parent 节点
      */
-    private void rightRotate(AVLTreeNode<V> node) {
+    private AVLTreeNode<V> rightRotate(AVLTreeNode<V> node) {
         /*
          右旋: 需要操作两个节点
              node: 不平衡的节点
@@ -97,6 +101,38 @@ public class AVLTree_base<V> {
         newParent.right = node;
         node.left = v;
 
+        node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+        newParent.height = Math.max(getHeight(newParent.left), getHeight(newParent)) + 1;
+
+        return newParent;
+    }
+
+    /**
+     * 左旋 处理 \ 型不平衡
+     *
+     * @param node 不平衡的节点, isBalanced(node) = false
+     * @return 新的 parent 节点
+     */
+    private AVLTreeNode<V> leftRotate(AVLTreeNode<V> node) {
+        /*
+         左旋: 需要操作两个节点
+             node: 不平衡的节点
+             newParent: 不平衡节点的右子节点
+         右旋过程:
+            1. 将 node 设置为 newParent 左子节点
+            2. 将 node 的右子节点设置为 newParent 原左子节点
+            3. 更新高度
+         树节点的变更, 至少要操作2个指针才能确定位置安全和线程安全, 因此无法使用CAS操作来保证线程安全
+         */
+        AVLTreeNode<V> newParent = node.right;
+        AVLTreeNode<V> v = newParent.left;
+        newParent.left = node;
+        node.right = v;
+
+        node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+        newParent.height = Math.max(getHeight(newParent.left), getHeight(newParent)) + 1;
+
+        return newParent;
     }
 
     private boolean isBalanced() {
@@ -134,6 +170,11 @@ public class AVLTree_base<V> {
         AVLTreeNode<V> left;
         AVLTreeNode<V> right;
         V v;
+        /*
+         树的高度: 从最低的叶子节点开始, 由0开始, 如果左右子树长度不一样, 则以最长的为准
+         区别于树的深度: 根节点的深度为0, 每个叶子节点的深度不一样
+         有的定义是从1开始
+         */
         int height;
         int balanceFactor;
 
--- algorithm-base/src/test/java/org/song/algorithm/algorithmbase/datatype/tree/model/TreeNode.java
+++
@@ -1,7 +0,0 @@
-//package org.song.algorithm.algorithmbase.datatype.tree.model;
-//
-//import java.io.Serializable;
-//
-//class TreeNode<V> implements Serializable {
-//    public V v;
-//}
