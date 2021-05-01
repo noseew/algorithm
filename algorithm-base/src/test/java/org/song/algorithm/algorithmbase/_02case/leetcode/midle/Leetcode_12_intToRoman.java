fbd499363eeb56a644aaca854184928a17e86387
noseew <noseew@qq.com>
2021-05-01 11:05:45 +0800

    leetcode

---
+++ algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02case/leetcode/midle/Leetcode_12_intToRoman.java
@@ -0,0 +1,65 @@
+package org.song.algorithm.algorithmbase._02case.leetcode.midle;
+
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * 12. 整数转罗马数字
+ * 罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+ * <p>
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+ * <p>
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+ * <p>
+ * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+ * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+ * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/integer-to-roman
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Leetcode_12_intToRoman {
+
+    @Test
+    public void test() {
+        System.out.println(intToRoman(1994));
+    }
+
+    public String intToRoman(int num) {
+
+        TreeMap<Integer, String> map = new TreeMap<>();
+        map.put(1, "I");
+        map.put(5, "V");
+        map.put(10, "X");
+        map.put(50, "L");
+        map.put(100, "C");
+        map.put(500, "D");
+        map.put(1000, "M");
+        map.put(4, "IV");
+        map.put(9, "IX");
+        map.put(40, "XL");
+        map.put(90, "XC");
+        map.put(400, "CD");
+        map.put(900, "CM");
+
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            Integer key = map.floorKey(num);
+            num -= key;
+            sb.append(map.get(key));
+        }
+
+        return sb.toString();
+    }
+}
