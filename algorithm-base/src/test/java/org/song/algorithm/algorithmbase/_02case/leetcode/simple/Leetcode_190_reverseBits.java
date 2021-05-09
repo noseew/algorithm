9b6da55ea9d4aff54699c9d0adf84002c1a84ea9
noseew <noseew@qq.com>
2021-05-09 12:41:42 +0800

    leetcode

---
+++ algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02case/leetcode/simple/Leetcode_190_reverseBits.java
@@ -0,0 +1,18 @@
+package org.song.algorithm.algorithmbase._02case.leetcode.simple;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 190. 颠倒二进制位
+ */
+public class Leetcode_190_reverseBits {
+
+    @Test
+    public void test() {
+        System.out.println("1 = " + reverseBits(1));
+    }
+
+    public int reverseBits(int n) {
+        return n;
+    }
+}
---
+++ algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02case/leetcode/simple/Leetcode_191_hammingWeight.java
@@ -0,0 +1,19 @@
+package org.song.algorithm.algorithmbase._02case.leetcode.simple;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 190. 颠倒二进制位
+ * 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+ */
+public class Leetcode_191_hammingWeight {
+
+    @Test
+    public void test() {
+        System.out.println("1 = " + hammingWeight(1));
+    }
+
+    public int hammingWeight(int n) {
+        return n;
+    }
+}
