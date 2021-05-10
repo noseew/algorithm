926a8ab5c5e47d820c1bc4c9ffcc5e649cd215bf
加一 <noseew@qq.com>
2021-05-10 17:47:19 +0800

    leecode

--- algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02case/leetcode/simple/Leetcode_168_convertToTitle.java
+++ algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02case/leetcode/simple/Leetcode_168_convertToTitle.java
@@ -10,12 +10,12 @@ public class Leetcode_168_convertToTitle {
 
     @Test
     public void test() {
-//        System.out.println("AAA = " + convertToTitle(703));
-//        System.out.println("ZY = " + convertToTitle(701));
-//        System.out.println("AB = " + convertToTitle(28));
-//        System.out.println("AA = " + convertToTitle(27));
-//        System.out.println("Z = " + convertToTitle(26));
-//        System.out.println("A = " + convertToTitle(1));
+        System.out.println("AAA = " + convertToTitle(703));
+        System.out.println("ZY = " + convertToTitle(701));
+        System.out.println("AB = " + convertToTitle(28));
+        System.out.println("AA = " + convertToTitle(27));
+        System.out.println("Z = " + convertToTitle(26));
+        System.out.println("A = " + convertToTitle(1));
         System.out.println("AZ = " + convertToTitle(52));
 
 //        System.out.println(703 % 26);
@@ -32,25 +32,22 @@ public class Leetcode_168_convertToTitle {
         if (columnNumber <= 0) {
             return "";
         }
+        if (columnNumber == 1) {
+            return parse(columnNumber) + "";
+        }
         StringBuilder sb = new StringBuilder();
         int n = columnNumber;
-        while (true) {
-            if (n > 26 ) {
-                if (n % 26 != 0) {
-                    sb.append(parse(n % 26));
-                } else {
-                    sb.append("Z");
-                }
-            } else {
-                sb.append(parse(n));
-                break;
-            }
+        while (n > 1) {
+            sb.append(parse(n % 26));
             n /= 26;
         }
         return sb.reverse().toString();
     }
 
     private char parse(int c) {
+        if (c == 0) {
+            return 'Z';
+        }
         // 将字符 1 转成 A, 字符 2 转成 B ...
         return (char) (c - 1 + 'A');
     }
