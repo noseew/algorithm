9e516727fd3369a4ab88498ff1a60b0349b7ca5d
noseew <noseew@qq.com>
2021-08-28 11:06:54 +0800

    leetcode

--- algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02case/leetcode/midle/Leetcode_11_maxArea.java
+++ algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02case/leetcode/midle/Leetcode_11_maxArea.java
@@ -2,6 +2,9 @@ package org.song.algorithm.algorithmbase._02case.leetcode.midle;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * 11. 盛最多水的容器
  * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
@@ -38,13 +41,33 @@ public class Leetcode_11_maxArea {
 
     @Test
     public void test() {
-        System.out.println(maxArea(new int[]{}));
+        System.out.println(maxArea2(new int[]{1, 1})); // 1
+        System.out.println(maxArea2(new int[]{4, 3, 2, 1, 4})); // 16
+        System.out.println(maxArea2(new int[]{1, 2, 1})); // 2
+    }
+
+    @Test
+    public void test2() {
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            int len = random.nextInt(100);
+            int[] height = new int[len];
+            for (int j = 0; j < len; j++) {
+                height[j] = random.nextInt(100);
+            }
+            if (maxArea(height) != maxArea2(height)) {
+                System.err.println("error" + Arrays.toString(height));
+            }
+        }
     }
 
     /**
      * 超时
      */
     public int maxArea(int[] height) {
+        if (height == null || height.length <= 1) {
+            return 0;
+        }
         int max = 0;
         for (int i = 0; i < height.length; i++) {
 
@@ -57,10 +80,26 @@ public class Leetcode_11_maxArea {
 
     }
 
+    /**
+     * 官方题解
+     *
+     * @param height
+     * @return
+     */
     public int maxArea2(int[] height) {
+        if (height == null || height.length <= 1) {
+            return 0;
+        }
         int max = 0;
-        int maxStartIndex = 0;
-        for (int i = 0; i < height.length; i++) {
+        int l = 0, r = height.length - 1;
+        while (l < r) {
+            int area = (r - l) * Math.min(height[l], height[r]);
+            max = Math.max(area, max);
+            if (height[l] <= height[r]) {
+                l++;
+            } else {
+                r--;
+            }
         }
         return max;
 
