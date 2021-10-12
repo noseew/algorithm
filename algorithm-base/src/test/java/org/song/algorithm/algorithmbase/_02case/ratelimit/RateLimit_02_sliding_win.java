d2de5750d361ec458110d66d6a226bbd4ea0c3a3
加一 <noseew@qq.com>
2021-10-13 00:27:54 +0800

    限流

--- algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02case/ratelimit/RateLimit_02_sliding_win.java
+++ algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02case/ratelimit/RateLimit_02_sliding_win.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
 
 public class RateLimit_02_sliding_win {
 
@@ -16,20 +17,5 @@ public class RateLimit_02_sliding_win {
 
     static class RateLimitSlidingWindow {
 
-        private final AtomicInteger count = new AtomicInteger();
-        private final int maxLimit;
-
-        private static ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-
-        public RateLimitSlidingWindow(int maxLimit, int seconds) {
-            this.maxLimit = maxLimit;
-            service.scheduleAtFixedRate(() -> {
-                count.set(0);
-            }, seconds, seconds, TimeUnit.SECONDS);
-        }
-
-        public boolean get() {
-            return count.getAndIncrement() <= maxLimit;
-        }
     }
 }
