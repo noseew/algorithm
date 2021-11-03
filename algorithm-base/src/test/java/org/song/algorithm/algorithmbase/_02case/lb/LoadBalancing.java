717ed6d12653574b2edbf7af897c0f421591df9a
noseew <noseew@qq.com>
2021-11-03 22:53:35 +0800

    lb

--- algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02case/lb/LoadBalancing.java
+++ algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02case/lb/LoadBalancing.java
@@ -7,7 +7,6 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -43,7 +42,6 @@ public class LoadBalancing {
             return select(tasks);
         }
     }
-
 
     @Test
     public void pollingLoadBalance() {
@@ -270,10 +268,21 @@ public class LoadBalancing {
         }
     }
 
+    /**
+     * Hash 调度
+     */
     @Test
     public void hashLoadBalance() {
         HashLoadBalance loadBalance = new HashLoadBalance();
-        for (int e : IntStream.range(1, 50).toArray()) {
+        for (int e : IntStream.range(1, 5).toArray()) {
+            loadBalance.select(tasks, e).invoke(e);
+        }
+        System.out.println("********************************");
+        for (int e : IntStream.range(1, 5).toArray()) {
+            loadBalance.select(tasks, e).invoke(e);
+        }
+        System.out.println("********************************");
+        for (int e : IntStream.range(1, 5).toArray()) {
             loadBalance.select(tasks, e).invoke(e);
         }
     }
@@ -286,20 +295,26 @@ public class LoadBalancing {
         }
     }
 
+    /**
+     * 一致性 Hash 调度
+     */
     @Test
     public void consistencyHashLoadBalance() {
         ConsistencyHashLoadBalance loadBalance = new ConsistencyHashLoadBalance();
-        for (int e : IntStream.range(1, 50).toArray()) {
-            if (e % 5 == 0) {
-                tasks.add(new Task("task" + e, 0));
-            }
+        System.out.println("*********************1**********************");
+        for (int e : IntStream.range(1, 10).toArray()) {
             loadBalance.select(tasks, e).invoke(e);
         }
-        System.out.println("*******************************************");
-        for (int e : IntStream.range(1, 50).toArray()) {
-            if (e % 5 == 0) {
-                tasks.removeIf(t -> t.getName().endsWith(String.valueOf(e)));
-            }
+//        tasks.add(new Task("task3", 0));
+        tasks.add(new Task("task4", 0));
+        System.out.println("*********************2**********************");
+        for (int e : IntStream.range(1, 10).toArray()) {
+            loadBalance.select(tasks, e).invoke(e);
+        }
+//        tasks.removeIf(t -> t.getName().equals("task3"));
+        tasks.removeIf(t -> t.getName().equals("task4"));
+        System.out.println("*********************3**********************");
+        for (int e : IntStream.range(1, 10).toArray()) {
             loadBalance.select(tasks, e).invoke(e);
         }
     }
@@ -312,94 +327,148 @@ public class LoadBalancing {
     Node02 [182-256]
      */
     static class ConsistencyHashLoadBalance implements LoadBalancer {
-
+        /**
+         * Hash环大小, 默认1024
+         */
         private final int range = 1 << 10;
-        private TreeMap<Integer, Node> hashRangeMap;
-        private Map<String, Node> hashKeyMap;
+        /**
+         * Hash环节点范围
+         * key=开始offset
+         */
+        private volatile TreeMap<Integer, Node> hashRingRange;
+        /**
+         * Hash环节点快捷访问Map
+         */
+        private Map<String, Node> hashKeyCache;
 
+        /**
+         * 确保Hash环是否需要调整
+         * 
+         * @param tasks
+         */
         private void ensure(List<Task> tasks) {
-            if (hashRangeMap == null) {
+            if (hashRingRange == null) {
                 initRing(tasks);
-                updateMap();
-            } else if (hashRangeMap.size() != tasks.size()){
+            }
+            if (hashRingRange.size() != tasks.size()){
                 adjustNode(tasks);
-                updateMap();
             }
         }
 
-        private void initRing(List<Task> tasks) {
-            hashRangeMap = new TreeMap<>();
+        /**
+         * 初始化Hash环和节点
+         * 
+         * @param tasks
+         */
+        private synchronized void initRing(List<Task> tasks) {
+            if (hashRingRange != null) {
+                return; // DCL
+            }
+            hashRingRange = new TreeMap<>();
             int step = range / tasks.size();
             int start = 0;
             for (Task task : tasks) {
                 Node node = new Node(task.getName(), start, start + step);
                 if (range - node.getEnd() < step) {
+                    // 如果是尾节点, 直接取到最后
                     node.setEnd(range);
                 }
-                hashRangeMap.put(start, node);
+                hashRingRange.put(start, node);
                 start += step;
             }
+            updateCache();
         }
 
-        private void updateMap() {
-            hashKeyMap = hashRangeMap.values().stream().collect(Collectors.toMap(Node::getKey, Function.identity(), (k1, k2) -> k1));
+        private void updateCache() {
+            hashKeyCache = hashRingRange.values().stream().collect(Collectors.toMap(Node::getKey, Function.identity(), (k1, k2) -> k1));
         }
 
         /**
+         * 调整Hash环
          */
         public synchronized void adjustNode(List<Task> tasks) {
+            if (hashRingRange.size() == tasks.size()){
+                return; // DCL
+            }
             Set<String> newTaskSet = tasks.stream().map(Task::getName).collect(Collectors.toSet());
-            Set<String> ringNodeSet = hashRangeMap.values().stream().map(Node::getKey).collect(Collectors.toSet());
+            Set<String> ringNodeSet = hashRingRange.values().stream().map(Node::getKey).collect(Collectors.toSet());
 
-            Set<String> removeSet = Sets.difference(ringNodeSet, newTaskSet);
-            Set<String> addSet = Sets.difference(newTaskSet, ringNodeSet);
+            // Hash环中需要删除的key
+            Set<String> needRemove = Sets.difference(ringNodeSet, newTaskSet);
+            // Hash环中需要添加的key
+            Set<String> needInsert = Sets.difference(newTaskSet, ringNodeSet);
 
-            if (!removeSet.isEmpty()) {
-                if (removeSet.size() == hashRangeMap.size()) {
+            if (!needRemove.isEmpty()) {
+                // 节点全部删除
+                if (needRemove.size() == hashRingRange.size()) {
+                    hashRingRange = null;
                     initRing(tasks);
                     return;
                 }
-                for (String key : removeSet) {
-                    removeNode(hashKeyMap.get(key));
+                // 节点部分删除
+                for (String key : needRemove) {
+                    removeNode(hashKeyCache.get(key));
                 }
             }
-            if (!addSet.isEmpty()) {
-                for (String key : addSet) {
-                    addNode(new Node(key, 0, 0));
+            if (!needInsert.isEmpty()) {
+                for (String key : needInsert) {
+                    insertNode(new Node(key, 0, 0));
                 }
             }
+            updateCache();
         }
 
-        private synchronized void removeNode(Node node) {
+        /**
+         * 删除Hash环中的节点
+         * 
+         * @param node
+         */
+        private void removeNode(Node node) {
             if (node == null) {
                 return;
             }
-            Map.Entry<Integer, Node> preNodeEntry = hashRangeMap.floorEntry(node.getStart() - 1);
-            Map.Entry<Integer, Node> nextNodeEntry = hashRangeMap.floorEntry(node.getEnd() + 1);
+            // 前一个节点
+            Map.Entry<Integer, Node> preNodeEntry = hashRingRange.floorEntry(node.getStart() - 1);
+            // 后一个节点
+            Map.Entry<Integer, Node> nextNodeEntry = hashRingRange.floorEntry(node.getEnd() + 1);
+            // 删除当前节点
+            hashRingRange.remove(node.getStart());
             if (preNodeEntry != null) {
+                // 删除后一个节点或者尾节点
+                // 直接修改前一个node的范围
                 preNodeEntry.getValue().setEnd(node.getEnd());
             } else if (nextNodeEntry != null) {
+                // 如果前一个node没有, 则说明删除的是头结点
                 nextNodeEntry.getValue().setStart(node.getStart());
             } else {
-                // 初始化 hashRangeMap
+                // 不可能走到这里, 初始化 hashRangeMap
             }
         }
 
-        private synchronized void addNode(Node node) {
-            Node maxRange = hashRangeMap.values().stream().max(Comparator.comparing(e -> e.getEnd() - e.getStart())).get();
-            int middle = (maxRange.getEnd() - maxRange.getStart()) / 2;
-            int oldEnd = maxRange.getEnd();
-            maxRange.setEnd(maxRange.getStart() + middle);
-            node.setStart(maxRange.getEnd());
+        /**
+         * 插入一个节点
+         * 
+         * @param node
+         */
+        private synchronized void insertNode(Node node) {
+            // 大节点 找到空隙最大的节点一分为二
+            Node maxNode = hashRingRange.values().stream().max(Comparator.comparing(e -> e.getEnd() - e.getStart())).get();
+            int middle = (maxNode.getEnd() - maxNode.getStart()) / 2;
+            int oldEnd = maxNode.getEnd();
+            // 大节点 放弃后半部分
+            maxNode.setEnd(maxNode.getStart() + middle);
+            // 新节点 使用大节点的后半部分
+            node.setStart(maxNode.getEnd());
             node.setEnd(oldEnd);
-            hashRangeMap.put(node.getStart(), node);
+            // 插入Node
+            hashRingRange.put(node.getStart(), node);
         }
 
         @Override
         public Task select(List<Task> tasks, Object param) {
             ensure(tasks);
             int i = System.identityHashCode(param) % range;
-            Node node = hashRangeMap.floorEntry(i).getValue();
+            Node node = hashRingRange.floorEntry(i).getValue();
             Map<String, Task> taskMap = tasks.stream()
                     .collect(Collectors.toMap(Task::getName, Function.identity(), (k1, k2) -> k1));
             return taskMap.getOrDefault(node.getKey(), tasks.get(0));
@@ -408,8 +477,11 @@ public class LoadBalancing {
         @Data
         @AllArgsConstructor
         static class Node {
+            // 节点和Task的对应标识
             private String key;
+            // 拥有hash环中开始的offset
             private int start;
+            // 拥有hash环中截止的offset
             private int end;
         }
     }
