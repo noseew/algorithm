a2b11a70707ac90f496a1a85f337f6d978f602e6
noseew <noseew@qq.com>
2022-01-16 18:46:23 +0800

    sort

--- algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_01base/sort/alg/Sort_Merge.java
+++ algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_01base/sort/alg/Sort_Merge.java
@@ -26,6 +26,39 @@ public class Sort_Merge {
 
         assert AbstractSort.isSorted(build);
     }
+    
+    public static abstract class AbstractMergeSort extends AbstractSort {
+
+        protected Comparable[] temp;
+
+        protected void merge(Comparable[] cs, int low, int mid, int hi) {
+            for (int k = low; k <= hi; k++) {
+                // 复制到临时数组, [low~hi]
+                temp[k] = cs[k];
+            }
+            /*
+            两个指针, 分别指的是两个有序数组的起始 index
+             */
+            int i = low, j = mid + 1;
+            for (int k = low; k <= hi; k++) {
+                // 归并到 cs
+                if (i > mid) {
+                    // 如果i到头了, 第一个数组用完了, 则直接使用第2个数组
+                    cs[k] = temp[j++];
+                } else if (j > hi) {
+                    // 如果j到头了, 第二个数组用完了, 则直接使用第1个数组
+                    cs[k] = temp[i++];
+                } else if (less(temp[j], temp[i])) {
+                    // j < i, 则将j合并, 同时j前进一步
+                    cs[k] = temp[j++];
+                } else {
+                    // 否则 i <= j, 将i合并, 同时i前进一步
+                    cs[k] = temp[i++];
+                }
+            }
+        }
+        
+    }
 
     /*
     归并排序最吸引人的性质是它能够保证将任意长度为N的数组排序所需时间和 NlogN 成正比
@@ -40,9 +73,7 @@ public class Sort_Merge {
     自顶向下的 merge
         逐个二分, 由前向后, 每有两个数组排好序就将其 merge 
      */
-    public static class MergeSort extends AbstractSort {
-
-        private Comparable[] temp;
+    public static class MergeSort extends AbstractMergeSort {
 
         @Override
         public void sort(Comparable[] cs) {
@@ -63,33 +94,6 @@ public class Sort_Merge {
             // 归并结果
             merge(cs, low, mid, hi);
         }
-
-        private void merge(Comparable[] cs, int low, int mid, int hi) {
-            for (int k = low; k <= hi; k++) {
-                // 复制到临时数组, [low~hi]
-                temp[k] = cs[k];
-            }
-            /*
-            两个指针, 分别指的是两个有序数组的起始 index
-             */
-            int i = low, j = mid + 1;
-            for (int k = low; k <= hi; k++) {
-                // 归并到 cs
-                if (i > mid) {
-                    // 
-                    cs[k] = temp[j++];
-                } else if (j > hi) {
-                    // 
-                    cs[k] = temp[i++];
-                } else if (less(temp[j], temp[i])) {
-                    // j < i, 则将j合并, 同时j前进一步
-                    cs[k] = temp[j++];
-                } else {
-                    // 否则 i <= j, 将i合并, 同时i前进一步
-                    cs[k] = temp[i++];
-                }
-            }
-        }
     }
 
     /*
@@ -97,9 +101,7 @@ public class Sort_Merge {
         逐个二分, 由前向后, 每两个数组都将其排好序, 然后分别两两进行merge 
 
      */
-    public static class MergeSort2 extends AbstractSort {
-
-        private Comparable[] temp;
+    public static class MergeSort2 extends AbstractMergeSort {
 
         @Override
         public void sort(Comparable[] cs) {
@@ -109,26 +111,6 @@ public class Sort_Merge {
             for (int sz = 1; sz < n; sz = sz + sz) {
                 for (int low = 0; low < n - sz; low += sz + sz) {
                     merge(cs, low, low + sz - 1, Math.min(low + sz + sz - 1, n - 1));
-                }
-            }
-        }
-
-        private void merge(Comparable[] cs, int low, int mid, int hi) {
-            int i = low, j = mid + 1;
-            for (int k = low; k <= hi; k++) {
-                // 复制到临时数组, [low~hi]
-                temp[k] = cs[k];
-            }
-            for (int k = low; k <= hi; k++) {
-                // 归并到 cs
-                if (i > mid) {
-                    cs[k] = temp[j++];
-                } else if (j > hi) {
-                    cs[k] = temp[i++];
-                } else if (less(temp[j], temp[i])) {
-                    cs[k] = temp[j++];
-                } else {
-                    cs[k] = temp[i++];
                 }
             }
         }
