7cfedb4439a15b9cf04ea2e2383704c903a73c4a
noseew <noseew@qq.com>
2022-02-07 21:42:04 +0800

    leetcode

--- algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02alg/_03app/leetcode/simple/Leetcode_94_inorderTraversal.java
+++ algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_02alg/_03app/leetcode/simple/Leetcode_94_inorderTraversal.java
@@ -3,6 +3,7 @@ package org.song.algorithm.algorithmbase._02alg._03app.leetcode.simple;
 import org.junit.Test;
 import org.song.algorithm.algorithmbase._02alg._03app.leetcode.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,11 +13,20 @@ public class Leetcode_94_inorderTraversal {
 
     @Test
     public void test() {
-        
     }
 
     public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorderTraversal(root, list);
+        return list;
+    }
 
-        return null;
+    public void inorderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, list);
+        list.add(root.val);
+        inorderTraversal(root.right, list);
     }
 }
