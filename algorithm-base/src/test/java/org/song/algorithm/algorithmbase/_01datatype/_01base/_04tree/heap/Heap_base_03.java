8f76f9ad0541bba85fcbdd10857052b891e8d0b6
noseew <noseew@qq.com>
2022-03-20 20:38:55 +0800

    heap

--- algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_01datatype/_01base/_04tree/heap/Heap_base_03.java
+++ algorithm-base/src/test/java/org/song/algorithm/algorithmbase/_01datatype/_01base/_04tree/heap/Heap_base_03.java
@@ -3,9 +3,16 @@ package org.song.algorithm.algorithmbase._01datatype._01base._04tree.heap;
 /**
  * heap 原地建堆
  * 3种方式 原地建堆
- * 1. 将数组数据逐个放入堆中
- * 2. 自上而下上滤
- * 3. 自下而上下滤
+ * 1. 将数组数据逐个放入堆中, 效率等价2
+ *  O(nlogn)
+ * 2. 自上而下上滤, 效率等价1
+ *  O(nlogn)所有节点深度之和?
+ * 3. 自下而上下滤, 
+ *  O(n)效率更高些, 
+ *      1)处理节点数较少, 
+ *      2)对比交换的总路程更短少, 所有节点高度之和?
+ *      
+ * 注意: 其他的(自上而下下滤/自下而上上滤) 都不行
  */
 public class Heap_base_03<T> extends Heap_base_02<T> {
 
@@ -29,21 +36,25 @@ public class Heap_base_03<T> extends Heap_base_02<T> {
 
     /**
      * 原地建堆, 2. 自上而下上滤
+     * 注意由于是直接操作数组, 所以要考虑size
      */
     public void build2() {
         for (int i = 0; i < datas.length; i++) {
             shiftUp(i);
         }
+//        size = datas.length;
     }
 
     /**
-     * 原地建堆, 3. 自下而上下滤
+     * 原地建堆, 3. 自下而上下滤 (推荐)
+     * 注意由于是直接操作数组, 所以要考虑size
      */
     public void build3() {
         int length = datas.length; // 忽略叶子结点
         for (int i = (length >> 1) - 1; i >= 0; i--) {
             shiftDown(i);
         }
+//        size = datas.length;
     }
 
 }
