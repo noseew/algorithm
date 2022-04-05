801ecc20b864689e5fe5445ca4c284afb2d27fc2
noseew <noseew@qq.com>
2022-04-05 16:25:15 +0800

    并查集

---
+++ algorithm-base/src/test/java/org/song/algorithm/base/_01datatype/_02high/unionfindsets/UnionFindSets.java
@@ -0,0 +1,10 @@
+package org.song.algorithm.base._01datatype._02high.unionfindsets;
+/*
+并查集
+UnionFindSets/DisjointSet
+
+这里的集合并不是指固定的数据结构set, 而是一组数据的集合, 他可以是多种数据结构
+ */
+public class UnionFindSets {
+    
+}
