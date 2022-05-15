960ec6ba227838559486162ec6ebd9d391d636ff
noseew <noseew@qq.com>
2022-05-15 21:14:17 +0800

    lfu

--- algorithm-base/src/test/java/org/song/algorithm/base/_01datatype/_03app/elimination/lfu/LFU02_CountMinSketch.java
+++ algorithm-base/src/test/java/org/song/algorithm/base/_01datatype/_03app/elimination/lfu/LFU02_CountMinSketch.java
@@ -3,6 +3,8 @@ package org.song.algorithm.base._01datatype._03app.elimination.lfu;
 import org.checkerframework.checker.index.qual.NonNegative;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class LFU02_CountMinSketch {
     /*
     
@@ -236,5 +238,107 @@ public class LFU02_CountMinSketch {
             return 1 << -Integer.numberOfLeadingZeros(x - 1);
         }
     }
-    
+
+    @Test
+    public void test02() {
+
+
+    }
+
+    static class CountMinRow {
+
+        private int countNum;
+        private int[] _bits;
+
+        public CountMinRow(int countNum) {
+            this.countNum = countNum;
+            _bits = new int[(countNum < 8 ? 8 : countNum) / 8];
+        }
+
+        public int get(int num) {
+            int countIndex = num / 8;
+            int countOffset = num % 8 * 4;
+            return (_bits[countIndex] >> countOffset) & 0x7;
+        }
+
+        //增加计数
+        void increment(int num) {
+            int countIndex = num / 8;
+            int countOffset = num % 8 * 4;
+
+            int value = (_bits[countIndex] >> countOffset) & 0x7;
+
+            if (value < 15) {
+                _bits[countIndex] += (1 << countOffset);
+            }
+        }
+
+        void clear() {
+            for (int i = 0; i < _bits.length; i++) {
+                _bits[i] = 0;
+            }
+        }
+
+        //保鲜算法，使计数值减半
+        void reset() {
+            for (int i = 0; i < _bits.length; i++) {
+                _bits[i] = (_bits[i] >> 1) & 0x77777777;
+            }
+        }
+
+    }
+
+    static class CountMinSketch {
+        private int countNum;
+        private int _mask; // 掩码用于取低n位
+        private int[] _seed; // 用于打散哈希值
+        private CountMinRow[] _cmRows;
+        static int COUNT_MIN_SKETCH_DEPTH = 4;
+
+        public CountMinSketch(int countNum) {
+            this.countNum = Integer.highestOneBit(countNum);
+            this._cmRows = new CountMinRow[this.countNum];
+            for (int i = 0; i < 4; i++) {
+                _cmRows[i] = new CountMinRow(this.countNum);
+            }
+            this._mask = countNum - 1;
+
+            _seed = new int[COUNT_MIN_SKETCH_DEPTH];
+            Random random = new Random();
+            for (int i = 0; i < COUNT_MIN_SKETCH_DEPTH; i++) {
+                _seed[i] = random.nextInt();
+            }
+        }
+
+        //选择几个cmRow最小的一个作为有效值返回
+        int getCountMin(int hash) {
+            int min = 16, value = 0;
+            for (int i = 0; i < _cmRows.length; i++) {
+                value = _cmRows[i].get((hash ^ _seed[i]) & _mask);
+
+                min = (value < min) ? value : min;
+            }
+            return min;
+        }
+
+        void Increment(int hash) {
+            for (int i = 0; i < _cmRows.length; i++) {
+                _cmRows[i].increment((hash ^ _seed[i]) & _mask);
+            }
+        }
+
+        void Reset() {
+            for (CountMinRow cmRow : _cmRows) {
+                cmRow.reset();
+            }
+        }
+
+        void Clear() {
+            for (CountMinRow cmRow : _cmRows) {
+                cmRow.clear();
+            }
+        }
+
+    }
+
 }
