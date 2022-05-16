63084ce3c967a6cbabdb29a978bccca51fec751e
noseew <noseew@qq.com>
2022-05-17 07:53:38 +0800

    lfu

--- algorithm-base/src/test/java/org/song/algorithm/base/_01datatype/_03app/elimination/lfu/LFU05_WTinyLFU.java
+++ algorithm-base/src/test/java/org/song/algorithm/base/_01datatype/_03app/elimination/lfu/LFU05_WTinyLFU.java
@@ -5,6 +5,9 @@ public class LFU05_WTinyLFU {
     /*
     caffeine 缓存淘汰算法
     
+    https://mp.weixin.qq.com/s/tKwPaYEKY0SkTglpl_l6dw
     https://segmentfault.com/a/1190000008751999
+    https://www.51cto.com/article/603434.html
+    https://blog.csdn.net/yingyujianmo/article/details/122755222
      */
 }
