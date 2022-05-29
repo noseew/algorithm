0e660ba394bb561805ebcf775f60ccca3e496ca1
noseew <noseew@qq.com>
2022-05-29 16:19:53 +0800

    并查集-路径分裂/减半

---
+++ algorithm-base/src/test/java/org/song/algorithm/base/_01datatype/_02high/unionfindsets/UFSQuickUnion_opt4_PathHalve.java
@@ -0,0 +1,42 @@
+package org.song.algorithm.base._01datatype._02high.unionfindsets;
+
+/*
+采用 quick union 思路实现
+
+一种优化
+路径压缩: 
+    针对find的优化
+    执行find操作的时候, find路径上的所有节点都指向根节点, 从而降低树的高度
+    降低了之后, 路径直接平铺成1层了, 成本比较高, 需要递归调用, 或需要额外的空间
+路径分裂: 
+    针对find的优化
+    执行find操作的时候, find路径上的所有节点都指向其祖父节点, 从而降低树的高度
+    降低了之后, 路径高度变成了原来的一半, 同时变成了两条, 不需要要递归和额外的空间
+路径减半: 这里采用此方式, 和路径分裂类似
+    针对find的优化
+    执行find操作的时候, find路径上的每隔一个节点都指向其祖父节点, 从而降低树的高度
+    降低了之后, 路径高度变成了原来的一半, 同时变成了一条主分支, 多个小的分支, 不需要要递归和额外的空间
+
+ */
+public class UFSQuickUnion_opt4_PathHalve extends UFSQuickUnion_opt2 {
+
+    
+    protected UFSQuickUnion_opt4_PathHalve(int capacity) {
+        super(capacity);
+    }
+
+    /*
+    递归查找父节点同时将当前父节点指向根节点
+     */
+    @Override
+    public int findRoot(int n) {
+        validRange(n);
+        while (parents[n] != n) {
+            parents[n] = parents[parents[n]];
+            n = parents[n];
+        }
+        // 返回最终的根节点
+        return parents[n];
+    }
+    
+}
---
+++ algorithm-base/src/test/java/org/song/algorithm/base/_01datatype/_02high/unionfindsets/UFSQuickUnion_opt4_PathSplit.java
@@ -0,0 +1,43 @@
+package org.song.algorithm.base._01datatype._02high.unionfindsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+采用 quick union 思路实现
+
+一种优化
+路径压缩: 
+    针对find的优化
+    执行find操作的时候, find路径上的所有节点都指向根节点, 从而降低树的高度
+    降低了之后, 路径直接平铺成1层了, 成本比较高, 需要递归调用, 或需要额外的空间
+路径分裂: 这里采用此方式
+    针对find的优化
+    执行find操作的时候, find路径上的所有节点都指向其祖父节点, 从而降低树的高度
+    降低了之后, 路径高度变成了原来的一半, 同时变成了两条, 不需要要递归和额外的空间
+路径减半:
+
+ */
+public class UFSQuickUnion_opt4_PathSplit extends UFSQuickUnion_opt2 {
+
+    
+    protected UFSQuickUnion_opt4_PathSplit(int capacity) {
+        super(capacity);
+    }
+
+    /*
+    递归查找父节点同时将当前父节点指向根节点
+     */
+    @Override
+    public int findRoot(int n) {
+        validRange(n);
+        while (parents[n] != n) {
+            int p = parents[n];
+            parents[n] = parents[parents[n]];
+            n = p;
+        }
+        // 返回最终的根节点
+        return parents[n];
+    }
+    
+}
