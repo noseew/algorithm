cce9e817bb95fd1822593c1477385b576e30dbea
noseew <noseew@qq.com>
2022-06-25 10:02:59 +0800

    queue

--- algorithm-base/src/test/java/org/song/algorithm/base/_01datatype/_01base/_02queue_stack/_01model/queue/test/QueueTest.java
+++ algorithm-base/src/test/java/org/song/algorithm/base/_01datatype/_01base/_02queue_stack/_01model/queue/test/QueueTest.java
@@ -4,14 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.song.algorithm.base._01datatype._01base._02queue_stack._01model.queue.Queue_Array_01;
 import org.song.algorithm.base._01datatype._01base._02queue_stack._01model.queue.Queue_Link_01;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class QueueTest {
 
-    private int maxVal = 100;
-    private int maxSize = 10;
+    private final int maxVal = 100;
+    private final int maxSize = 10;
 
     private Random r = new Random();
     
@@ -19,12 +22,12 @@ public class QueueTest {
     public void queueList_test01() throws InterruptedException {
 
         Queue_Link_01<Integer> queue = new Queue_Link_01<>();
-        LinkedBlockingQueue<Integer> q = new LinkedBlockingQueue<>();
+        Queue<Integer> q = new LinkedList<>();
 
         for (int i = 0; i < maxSize; i++) {
             int val = r.nextInt(maxVal);
             queue.lpush(val);
-            q.put(val);
+            q.offer(val);
         }
 
         assert queue.length() == q.size();
@@ -38,12 +41,12 @@ public class QueueTest {
     public void queueArray_test01() throws InterruptedException {
 
         Queue_Array_01<Integer> queue = new Queue_Array_01<>();
-        ArrayBlockingQueue<Integer> q = new ArrayBlockingQueue<>(maxSize);
+        Queue<Integer> q = new LinkedList<>();
 
         for (int i = 0; i < maxSize; i++) {
             int val = r.nextInt(maxVal);
             queue.lpush(val);
-            q.put(val);
+            q.offer(val);
         }
 
         assert queue.length() == q.size();
