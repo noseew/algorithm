9e7090fda0f813e4a680cda92753109ca9687c92
noseew <noseew@qq.com>
2022-08-01 22:26:02 +0800

    dp

--- algorithm-base/src/test/java/org/song/algorithm/base/_02alg/thinking/_05dp/_04LCS2.java
+++ algorithm-base/src/test/java/org/song/algorithm/base/_02alg/thinking/_05dp/_04LCS2.java
@@ -1,18 +1,52 @@
 package org.song.algorithm.base._02alg.thinking._05dp;
 
+import org.junit.jupiter.api.Test;
+
 /**
- * 最长公共子序列
+ * 最长公共子序列, 不连续
  */
-public class LCS2 {
-    public static void main(String[] args) {
+public class _04LCS2 {
+
+    @Test
+    public void main() {
         int len = lcs3(new int[]{1, 3, 5, 9, 10}, new int[]{1, 4, 9, 10});
         System.out.println(len);
     }
 
     /**
+     * 递归实现
+     * 暴力递归
+     */
+    int lcs1(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0) return 0; // 检测非法数据
+        if (nums2 == null || nums2.length == 0) return 0; // 检测非法数据
+        return lcs1(nums1, nums1.length, nums2, nums2.length);
+    }
+
+    /**
+     * 求nums1前i个元素和nums2前j个元素的最长g公共子序列长度
+     *
+     * @param nums1
+     * @param i
+     * @param nums2
+     * @param j
+     */
+    int lcs1(int[] nums1, int i, int[] nums2, int j) {
+        if (i == 0 || j == 0) return 0;
+        // 最后一个元素相等, 返回前面的公共子序列长度 + 1
+        if (nums1[i - 1] == nums2[j - 1]) {
+            return lcs1(nums1, i - 1, nums2, j - 1) + 1;
+        }
+        return Math.max(
+                lcs1(nums1, i - 1, nums2, j),
+                lcs1(nums1, i, nums2, j - 1)
+        );
+    }
+
+    /**
      * 非递归实现(滚动数组优化)
      */
-    static int lcs3(int[] nums1, int[] nums2) {
+    int lcs3(int[] nums1, int[] nums2) {
         if (nums1 == null || nums1.length == 0) return 0;
         if (nums2 == null || nums2.length == 0) return 0;
         int[][] dp = new int[2][nums2.length + 1];
@@ -33,7 +67,7 @@ public class LCS2 {
     /**
      * 非递归实现
      */
-    static int lcs2(int[] nums1, int[] nums2) {
+    int lcs2(int[] nums1, int[] nums2) {
         if (nums1 == null || nums1.length == 0) return 0;
         if (nums2 == null || nums2.length == 0) return 0;
         int[][] dp = new int[nums1.length + 1][nums2.length + 1];
@@ -47,34 +81,5 @@ public class LCS2 {
             }
         }
         return dp[nums1.length][nums2.length];
-    }
-
-    /**
-     * 递归实现
-     */
-    static int lcs1(int[] nums1, int[] nums2) {
-        if (nums1 == null || nums1.length == 0) return 0; // 检测非法数据
-        if (nums2 == null || nums2.length == 0) return 0; // 检测非法数据
-        return lcs1(nums1, nums1.length, nums2, nums2.length);
-    }
-
-    /**
-     * 求nums1前i个元素和nums2前j个元素的最长g公共子序列长度
-     *
-     * @param nums1
-     * @param i
-     * @param nums2
-     * @param j
-     */
-    static int lcs1(int[] nums1, int i, int[] nums2, int j) {
-        if (i == 0 || j == 0) return 0;
-        // 最后一个元素相等, 返回前面的公共子序列长度 + 1
-        if (nums1[i - 1] == nums2[j - 1]) {
-            return lcs1(nums1, i - 1, nums2, j - 1) + 1;
-        }
-        return Math.max(
-                lcs1(nums1, i - 1, nums2, j),
-                lcs1(nums1, i, nums2, j - 1)
-        );
     }
 }
--- algorithm-base/src/test/java/org/song/algorithm/base/_02alg/thinking/_05dp/_05LCSubstring.java
+++ algorithm-base/src/test/java/org/song/algorithm/base/_02alg/thinking/_05dp/_05LCSubstring.java
@@ -1,44 +1,38 @@
 package org.song.algorithm.base._02alg.thinking._05dp;
 
-public class LCSubstring {
-    public static void main(String[] args) {
+import org.junit.jupiter.api.Test;
+
+/**
+ * 最长公共子串, 连续
+ */
+public class _05LCSubstring {
+
+    @Test
+    public void main() {
         System.out.println(lcs1("ABCDE", "ABC"));
         System.out.println(lcs2("ABCDE", "ABC"));
         System.out.println(lcs3("ABCDE", "ABC"));
     }
 
-    static int lcs3(String str1, String str2) {
+    int lcs1(String str1, String str2) {
         if (str1 == null || str2 == null) return 0;
         char[] chars1 = str1.toCharArray();
         if (chars1.length == 0) return 0;
         char[] chars2 = str2.toCharArray();
         if (chars2.length == 0) return 0;
-        // 选取长度较短的作为列
-        int rowsLength = chars1.length;
-        int colsLength = chars2.length;
-        if (chars1.length < chars2.length) {
-            colsLength = chars1.length;
-            rowsLength = chars2.length;
-        }
-        int[] dp = new int[colsLength + 1];
+        int[][] dp = new int[chars1.length + 1][chars2.length + 1];
         int max = 0;
-        for (int row = 1; row <= rowsLength; row++) {
-            int cur = 0;
-            for (int col = 1; col <= colsLength; col++) {
-                int leftTop = cur;
-                cur = dp[col];
-                if (chars1[row - 1] != chars2[col - 1]) {
-                    dp[col] = 0;
-                } else {
-                    dp[col] = leftTop + 1;
-                    max = Math.max(max, dp[col]);
-                }
+        for (int i = 1; i <= chars1.length; i++) {
+            for (int j = 1; j <= chars2.length; j++) {
+                if (chars1[i - 1] != chars2[j - 1]) continue;
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                max = Math.max(max, dp[i][j]);
             }
         }
         return max;
     }
 
-    static int lcs2(String str1, String str2) {
+    int lcs2(String str1, String str2) {
         if (str1 == null || str2 == null) return 0;
         char[] chars1 = str1.toCharArray();
         if (chars1.length == 0) return 0;
@@ -65,19 +59,32 @@ public class LCSubstring {
         return max;
     }
 
-    static int lcs1(String str1, String str2) {
+    int lcs3(String str1, String str2) {
         if (str1 == null || str2 == null) return 0;
         char[] chars1 = str1.toCharArray();
         if (chars1.length == 0) return 0;
         char[] chars2 = str2.toCharArray();
         if (chars2.length == 0) return 0;
-        int[][] dp = new int[chars1.length + 1][chars2.length + 1];
+        // 选取长度较短的作为列
+        int rowsLength = chars1.length;
+        int colsLength = chars2.length;
+        if (chars1.length < chars2.length) {
+            colsLength = chars1.length;
+            rowsLength = chars2.length;
+        }
+        int[] dp = new int[colsLength + 1];
         int max = 0;
-        for (int i = 1; i <= chars1.length; i++) {
-            for (int j = 1; j <= chars2.length; j++) {
-                if (chars1[i - 1] != chars2[j - 1]) continue;
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                max = Math.max(max, dp[i][j]);
+        for (int row = 1; row <= rowsLength; row++) {
+            int cur = 0;
+            for (int col = 1; col <= colsLength; col++) {
+                int leftTop = cur;
+                cur = dp[col];
+                if (chars1[row - 1] != chars2[col - 1]) {
+                    dp[col] = 0;
+                } else {
+                    dp[col] = leftTop + 1;
+                    max = Math.max(max, dp[col]);
+                }
             }
         }
         return max;
