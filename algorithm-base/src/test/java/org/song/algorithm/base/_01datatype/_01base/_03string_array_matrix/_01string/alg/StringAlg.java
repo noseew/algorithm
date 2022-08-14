2a2d5b3f06bd242a64570603e79b5b4a83655bd1
noseew <noseew@qq.com>
2022-08-14 12:21:35 +0800

    doc

---
+++ algorithm-base/src/test/java/org/song/algorithm/base/_01datatype/_01base/_03string_array_matrix/_01string/alg/StringAlg.java
@@ -0,0 +1,16 @@
+package org.song.algorithm.base._01datatype._01base._03string_array_matrix._01string.alg;
+
+public class StringAlg {
+    
+    /*
+    字符串匹配算法
+    
+    1. 暴力
+    2. KMP
+    3. Boyer-Moore(BM)
+    4. Rabin-Karp(RK)
+    5. Sunday
+    6. AC自动机
+     */
+    
+}
