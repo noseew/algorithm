f6f8c5aca4116e5c64ad503c098f45bc9103e280
Galileo <20148179+noseew@users.noreply.github.com>
2022-08-22 21:17:33 +0800

    base alg 二分

--- algorithm-base/src/test/java/org/song/algorithm/base/_02alg/common/ArrayBase.java
+++ algorithm-base/src/test/java/org/song/algorithm/base/_02alg/common/ArrayBase.java
@@ -1,15 +1,40 @@
 package org.song.algorithm.base._02alg.common;
 
+import org.junit.jupiter.api.Test;
+
 public class ArrayBase {
 
+    @Test
     public void test01() {
-
+        System.out.println("1 = " + mid(new int[]{1}, 1));
+        System.out.println("-1 = " + mid(new int[]{1}, -1));
+        System.out.println("1 = " + mid(new int[]{1, 2, 3}, 1));
+        System.out.println("3 = " + mid(new int[]{1, 2, 3}, 3));
+        System.out.println("-1 = " + mid(new int[]{1, 2, 3}, -1));
+        
+        System.out.println("-1 = " + mid(new int[]{1, 2, 3, 5}, -1));
+        System.out.println("3 = " + mid(new int[]{1, 2, 3, 4, 5}, 3));
     }
 
     /**
      * 二分法取中
      */
-    public int reversal1(int[] nums) {
-        return 0;
+    public int mid(int[] nums, int v) {
+        return mid(nums, v, 0, nums.length);
+    }
+
+    public int mid(int[] nums, int v, int l, int r) {
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (v < nums[mid]) {
+                r = mid;
+            } else if (v > nums[mid]) {
+                l = mid + 1;
+            } else {
+                return nums[mid];
+            }
+        }
+        return -1;
     }
 }
