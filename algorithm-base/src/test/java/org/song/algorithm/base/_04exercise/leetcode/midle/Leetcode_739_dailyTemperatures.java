e048954515aaa794b4a7efaaf3ae88515efd792e
Galileo <20148179+noseew@users.noreply.github.com>
2022-08-26 21:56:20 +0800

    Leetcode_739

--- algorithm-base/src/test/java/org/song/algorithm/base/_04exercise/leetcode/midle/Leetcode_739_dailyTemperatures.java
+++ algorithm-base/src/test/java/org/song/algorithm/base/_04exercise/leetcode/midle/Leetcode_739_dailyTemperatures.java
@@ -19,16 +19,16 @@ public class Leetcode_739_dailyTemperatures {
     @Test
     public void test() {
         int[] treeNode = constructMaximumBinaryTree(new int[]{3, 2, 1, 6, 0, 5});
-        System.out.println(Arrays.toString(treeNode));
+        System.out.println(Arrays.toString(treeNode)); // [3, 2, 1, 0, 1, 0]
 
         int[] treeNode2 = constructMaximumBinaryTree(new int[]{73, 74, 75, 71, 69, 72, 76, 73});
-        System.out.println(Arrays.toString(treeNode2));
+        System.out.println(Arrays.toString(treeNode2)); // [1, 1, 4, 2, 1, 1, 0, 0]
 
         int[] treeNode3 = constructMaximumBinaryTree(new int[]{30, 40, 50, 60});
-        System.out.println(Arrays.toString(treeNode3));
+        System.out.println(Arrays.toString(treeNode3)); // [1, 1, 1, 0]
 
         int[] treeNode4 = constructMaximumBinaryTree(new int[]{30, 60, 90});
-        System.out.println(Arrays.toString(treeNode4));
+        System.out.println(Arrays.toString(treeNode4)); // [1, 1, 0]
     }
 
     /**
@@ -62,8 +62,30 @@ public class Leetcode_739_dailyTemperatures {
         return rarray;
     }
 
+    @Test
+    public void test2() {
+        int[] treeNode = constructMaximumBinaryTree2(new int[]{3, 2, 1, 6, 0, 5});
+        System.out.println(Arrays.toString(treeNode)); // [3, 2, 1, 0, 1, 0]
+
+        int[] treeNode2 = constructMaximumBinaryTree2(new int[]{73, 74, 75, 71, 69, 72, 76, 73});
+        System.out.println(Arrays.toString(treeNode2)); // [1, 1, 4, 2, 1, 1, 0, 0]
+
+        int[] treeNode3 = constructMaximumBinaryTree2(new int[]{30, 40, 50, 60});
+        System.out.println(Arrays.toString(treeNode3)); // [1, 1, 1, 0]
+
+        int[] treeNode4 = constructMaximumBinaryTree2(new int[]{30, 60, 90});
+        System.out.println(Arrays.toString(treeNode4)); // [1, 1, 0]
+
+        int[] treeNode5 = constructMaximumBinaryTree2(new int[]{89, 62, 70, 58, 47, 47, 46, 76, 100, 70});
+        System.out.println(Arrays.toString(treeNode5)); // [8,1,5,4,3,2,1,1,0,0]
+
+        int[] treeNode6 = constructMaximumBinaryTree2(new int[]{34, 80, 80, 34, 34, 80, 80, 80, 80, 34});
+        System.out.println(Arrays.toString(treeNode6)); // [1,0,0,2,1,0,0,0,0,0]
+    }
+
     /**
-     * 思路2, 采用类似动态规划思想 TODO
+     * 思路2, 采用类似动态规划思想
+     * 从后往前, 依次记录
      */
     public int[] constructMaximumBinaryTree2(int[] temperatures) {
         if (temperatures == null || temperatures.length == 0) {
@@ -71,6 +93,20 @@ public class Leetcode_739_dailyTemperatures {
         }
         // 右边第一个比其大的位置
         int[] rarray = new int[temperatures.length];
+        for (int i = temperatures.length - 2; i >= 0; i--) {
+            int nextBiggerIndex = i + 1;
+            // 找到后一个比其更大的元素下标位置
+            while (rarray[nextBiggerIndex] != 0 && temperatures[nextBiggerIndex] < temperatures[i]) {
+                nextBiggerIndex += rarray[nextBiggerIndex];
+            }
+            if (temperatures[nextBiggerIndex] > temperatures[i]) {
+                // 找到了, 直接赋值
+                rarray[i] = nextBiggerIndex - i;
+            } else if (temperatures[nextBiggerIndex] == temperatures[i] && rarray[nextBiggerIndex] != 0) {
+                // 处理相等值的情况
+                rarray[i] = rarray[nextBiggerIndex] + nextBiggerIndex - i;
+            }
+        }
         return rarray;
     }
 
