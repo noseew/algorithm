39c4c9130c6d4e528b59a7df27ed46b2fa4d4950
Galileo <noseew@gmail.com>
2022-09-11 18:18:35 +0800

    Leetcode_70

--- algorithm-base/src/test/java/org/song/algorithm/base/_04exercise/leetcode/simple/Leetcode_70_ClimbStairs.java
+++ algorithm-base/src/test/java/org/song/algorithm/base/_04exercise/leetcode/simple/Leetcode_70_ClimbStairs.java
@@ -37,7 +37,20 @@ public class Leetcode_70_ClimbStairs {
         System.out.println(climbStairs(10));
     }
 
+    /**
+     * 采用递归, 超时时间限制
+     */
     public int climbStairs(int n) {
+        if (n <= 2) return n;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+
+    }
+
+    /**
+     * 动态规划
+     * 参见: _02ClimbStair
+     */
+    public int climbStairs2(int n) {
         int p = 0, q = 0, r = 1;
         for (int i = 1; i <= n; ++i) {
             p = q;
