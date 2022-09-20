de239977be2f1b7bab77f4672da20ef3f501f9eb
noseew <noseew@gmail.com>
2022-09-20 22:28:20 +0800

    Leetcode_55

--- algorithm-base/src/test/java/org/song/algorithm/base/_04exercise/leetcode/midle/Leetcode_55_canJump.java
+++ algorithm-base/src/test/java/org/song/algorithm/base/_04exercise/leetcode/midle/Leetcode_55_canJump.java
@@ -21,6 +21,10 @@ public class Leetcode_55_canJump {
         System.out.println(canJump(new int[]{2, 5, 0, 0})); // true ???
     }
 
+    /**
+     * 思路不对, 每一步最多跳[i]步, 而不是只能跳[i]步
+     * 未完成
+     */
     public boolean canJump(int[] nums) {
         if (nums.length == 1) return true;
 
@@ -33,5 +37,48 @@ public class Leetcode_55_canJump {
             start = start + num;
         }
         return start >= last;
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(canJump2(new int[]{2, 3, 1, 1, 4})); // true
+        jumped = false;
+        System.out.println(canJump2(new int[]{3, 2, 1, 0, 4})); // false
+        jumped = false;
+        System.out.println(canJump2(new int[]{2, 0})); // true
+        jumped = false;
+        System.out.println(canJump2(new int[]{2, 5, 0, 0})); // true ???
+        jumped = false;
+    }
+
+    /**
+     * 采用暴力递归
+     * 超出时间限制
+     */
+    public boolean canJump2(int[] nums) {
+        if (nums.length == 1) return true;
+
+        jump(nums, 0, nums[0]);
+        return jumped;
+    }
+
+    private boolean jumped = false;
+
+    /**
+     * @param nums
+     * @param currentIdx 当前位置
+     * @param maxStep    下一步能跳到最远位置
+     */
+    private void jump(int[] nums, int currentIdx, int maxStep) {
+        if (jumped) return;
+//        System.out.printf("currentIdx=%s, maxStep=%s, jumped=%s \r\n", currentIdx, maxStep, jumped);
+        if (currentIdx + maxStep >= nums.length - 1) {
+            jumped = true;
+            return;
+        }
+        for (int step = maxStep; step > 0; step--) {
+            int nextIdx = currentIdx + step;
+            jump(nums, nextIdx, nums[nextIdx]);
+        }
     }
 }
